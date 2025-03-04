41ecaa6837c1c8c7f2986de747dca3e329fb10f9
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-03-05 12:52:35 +1300

    Imported and applied ttk and introduced columnspan

--- temperature.py
+++ temperature.py
@@ -1,23 +1,24 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 
-title = tk.Label(root, text="Temperature\nConverter")
-title.grid(row = 0, column = 0)
+title = ttk.Label(root, text="Temperature\nConverter")
+title.grid(row = 0, column = 0, columnspan = 2)
 
-number = tk.Entry(root)
-number.grid(row = 1, column = 0)
+number = ttk.Entry(root)
+number.grid(row = 1, column = 0, columnspan = 2)
 
-c_to_f_rb = tk.Radiobutton(root, text="C to F", value=1)
+c_to_f_rb = ttk.Radiobutton(root, text="C to F", value=1)
 c_to_f_rb.grid(row = 2, column = 0)
 
-f_to_c_rb = tk.Radiobutton(root, text="F to C", value=2)
+f_to_c_rb = ttk.Radiobutton(root, text="F to C", value=2)
 f_to_c_rb.grid(row = 2, column = 1)
 
-convert_b = tk.Button(root, text="Convert")
-convert_b.grid(row = 3, column = 0)
+convert_b = ttk.Button(root, text="Convert")
+convert_b.grid(row = 3, column = 0, columnspan = 2)
 
-result_l = tk.Label(root, text="")
-result_l.grid(row = 4, column = 0)
+result_l = ttk.Label(root, text="")
+result_l.grid(row = 4, column = 0, columnspan = 2)
 
 root.mainloop()

21cbe4b1fb349f1f4957944767780140e29febe1
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-03-10 10:31:40 +1300

    Added functions for every button and label except for choice to finish and refine game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,20 +8,62 @@ root.title("Rock Paper Scissors Game")
 
 options = ["Rock", "Paper", "Scissors"]
 
+score = 0
+computer_score = 0
+
+def ai_choice():
+    return random.choice(options)
+
 def rock():
-    result_text.config(text = "")
+    ai = ai_choice()
+    if ai == "Rock":
+        result_text.config(text = "Tie")
+    elif ai == "Paper":
+        result_text.config(text = "You lose")
+    elif ai == "Scissors":
+        result_text.config(text = "You win")
+    score_count()
 
 def paper():
-    result_text.config(text = "")
+    ai = ai_choice()
+    if ai == "Paper":
+        result_text.config(text = "Tie")
+    elif ai == "Scissors":
+        result_text.config(text = "You lose")
+    elif ai == "Rock":
+        result_text.config(text = "You win")
+    score_count()
 
 def scissors():
-    result_text.config(text = "")
+    ai = ai_choice()
+    if ai == "Scissors":
+        result_text.config(text = "Tie")
+    elif ai == "Rock":
+        result_text.config(text = "You lose")
+    elif ai == "Paper":
+        result_text.config(text = "You win")
+    score_count()
+
+def score_count():
+    global score, computer_score
+    if result_text.cget("text") == "You win":
+        score += 1
+        score_text.config(text = "Score: " + str(score))
+    elif result_text.cget("text") == "You lose":
+        computer_score += 1
+        computer_score_text.config(text = "Computer score: " + str(computer_score))
 
 choice_text = ttk.Label(root, text = "Choose one:")
 choice_text.grid(row = 0, column = 1, padx = 1, pady = 1)
 
 result_text = ttk.Label(root, text = "Result:")
-result_text.grid(row = 2, column = 1, padx = 1, pady = 1)
+result_text.grid(row = 3, column = 1, padx = 1, pady = 1)
+
+score_text = ttk.Label(root, text = "Score: 0")
+score_text.grid(row = 4, column = 1, padx = 1, pady = 1)
+
+computer_score_text = ttk.Label(root, text = "Computer score: 0")
+computer_score_text.grid(row = 5, column = 1, padx = 1, pady = 1)
 
 rock_button = ttk.Button(root, text = "Rock", command = rock)
 rock_button.grid(row = 1, column = 0, padx = 1, pady = 1)

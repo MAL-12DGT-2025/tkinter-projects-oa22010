fb93783f144dd28fa9af98e6083d41031f0d41c7
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-03-18 15:12:49 +1300

    Completed first draft of bible verse python file

--- bible_verse.py
+++ bible_verse.py
@@ -4,10 +4,23 @@ from tkinter import ttk
 root = tk.Tk()
 root.title('Favorite Bible Verse')
 
-verse = open('bible_verse.txt')
-verse.write()
+def confirm():
+    verse = user_input.get()
+    with open('bible_verse.txt', 'w') as file:
+        file.write(verse)
+    read_verse = open('bible_verse.txt').read()
+    verse_display.config(text = f'Bible verse: {read_verse}')
 
-user_input = ttk.Label(root, text='Favorite Bible Verse')
-user_input.grid(row = 0, column = 0)
+verse_text = ttk.Label(root, text = 'Insert favorite Bible verse:')
+verse_text.pack(pady = 1, padx = 5)
+
+user_input = ttk.Entry(root, text = '(Insert here)')
+user_input.pack(pady = 1)
+
+confirm_button = ttk.Button(root, text = 'Confirm', command  = confirm)
+confirm_button.pack(pady = 1)
+
+verse_display = ttk.Label(root, text = 'Bible verse:')
+verse_display.pack(pady = 1)
 
 root.mainloop()

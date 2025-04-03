c9dcc5598f9004f27b878fcd9c9f8b0b7d3228c4
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-04-04 10:01:54 +1300

    Did some tests on tkinter

--- Others_folder/tkinter_tests.py
+++ Others_folder/tkinter_tests.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 
 root = tk.Tk()
 
-tk.Spinbox(root, from_=0, to=100000000000).pack()
-
+spinbox = tk.Spinbox(root, from_=0, to=100000000000, increment=1)
+spinbox.grid(row=0, column=0, padx=10, pady=10)
+spinbox.bind("<FocusIn>", lambda event: spinbox.select_range(0, tk.END))
+spinbox.bind("<FocusOut>", lambda event: spinbox.select_clear(0, tk.END))
 
 root.mainloop()

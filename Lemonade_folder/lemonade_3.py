f0914e202f842a5bd7eeeb9d2a41fddf4b22b0e4
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-04-07 13:42:17 +1200

    Made reccomended edits to lemonade flowchart

--- Lemonade_folder/lemonade_3.py
+++ Lemonade_folder/lemonade_3.py
@@ -27,13 +27,13 @@ pink_var = tk.StringVar(value="0")
 large_var = tk.StringVar(value="0")
 
 # Create and place the widgets
-add_text = ttk.Label(root, text="Items:")
+add_text = ttk.Label(root, text="Items:", anchor="center")
 add_text.grid(row=0, column=0, padx=10, pady=10)
 
-less_text = ttk.Label(root, text="Amount:")
+less_text = ttk.Label(root, text="Amount:", anchor="center")
 less_text.grid(row=0, column=1, padx=10, pady=10)
 
-cost_text = ttk.Label(root, text="Cost each:")
+cost_text = ttk.Label(root, text="Cost each:", anchor="center")
 cost_text.grid(row=0, column=3, padx=10, pady=10)
 
 lemonade_button = ttk.Spinbox(root, from_=0, to=100000000000, increment=1, textvariable=lemonade_var, command=calculate)
@@ -51,19 +51,19 @@ pink_button.grid(row=4, column=1, padx=1, pady=1)
 large_button = ttk.Spinbox(root, from_=0, to=100000000000, increment=1, textvariable=large_var, command=calculate)
 large_button.grid(row=5, column=1, padx=1, pady=1)
 
-amount_enter_a = ttk.Label(root, text="Lemonade", width=12)
+amount_enter_a = ttk.Label(root, text="Lemonade", width=12, anchor="center")
 amount_enter_a.grid(row=1, column=0, padx=5, pady=1)
 
-amount_enter_b = ttk.Label(root, text="Pink Flavor", width=12)
+amount_enter_b = ttk.Label(root, text="Pink Flavor", width=12, anchor="center")
 amount_enter_b.grid(row=2, column=0, padx=5, pady=1)
 
-amount_enter_c = ttk.Label(root, text="Large cup", width=12)
+amount_enter_c = ttk.Label(root, text="Large cup", width=12, anchor="center")
 amount_enter_c.grid(row=3, column=0, padx=5, pady=1)
 
-amount_enter_d = ttk.Label(root, text="Straw", width=12)
+amount_enter_d = ttk.Label(root, text="Straw", width=12, anchor="center")
 amount_enter_d.grid(row=4, column=0, padx=5, pady=1)
 
-amount_enter_e = ttk.Label(root, text="Ice", width=12)
+amount_enter_e = ttk.Label(root, text="Ice", width=12, anchor="center")
 amount_enter_e.grid(row=5, column=0, padx=5, pady=1)
 
 cost_text_a = ttk.Label(root, text="$2")
